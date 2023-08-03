ed255f00d4e8d31c4993f2b4d92ba5c1b28ef0b9
Tingfeng <pangtingfeng99@gmail.com>
2023-08-03 13:02:59 +0800

    UploadTeacher csv implemented

--- src/school_admin_module/UploadTeacher.js
+++ src/school_admin_module/UploadTeacher.js
@@ -8,12 +8,18 @@ import {
 import { useState } from 'react'
 import { Card, CardHeader, Typography } from '@material-tailwind/react';
 import '../css/defaultstyle.css'
+import axios from 'axios';
 
 export default function UploadTeacher() {
+  // PREVIEW START //
+  const [tableVisible, setTableVisible] = useState(false)
+  const handlePreview = () => {
+    setTableVisible(!tableVisible)
+  }
+  // PREVIEW END //
+
   //  UPLOAD FUNCTION START  //
-  //
-  // Papaparse codes
-  // State to store csv data
+  // States to store csv data
   const [ data, setData ] = useState([]);
   const [ column, setColumn ] = useState([]);
   const [ value, setValue ] = useState([]);
@@ -31,21 +37,21 @@ export default function UploadTeacher() {
             // Check if all required values are present in the row
             const keys = Object.keys(d);
             const values = Object.values(d);
-            const allValuesPresent = values.every((val) => val !== "");
-    
+            const allValuesPresent = values.every((val) => val !== undefined && val !== "");  // check if value is either empty string or undefined
+            
+            // Only process rows that have complete values
             if (allValuesPresent) {
               column.push(keys);
               value.push(values);
             }
-            // If any value is missing, skip this row and consider it invalid
-            // Rows with missing values will not be pushed into column and value arrays.
           });
 
           setData(result.data);
           setColumn(column[0]);
-          setValue(value);
-          setNumberOfRows(value.length) // To see number of rows uploaded
 
+          // To see number of rows uploaded
+          setValue(value);
+          setNumberOfRows(value.length) 
         }
       }
     )
@@ -55,11 +61,54 @@ export default function UploadTeacher() {
       if (data.length == 0) {
         alert('Upload csv file first')
       } else {
-        alert('Clicked submit')
-        window.location.reload()
+        // First validation, check if user submitted csv file with correct headers 
+        // For upload teachers, header should strictly be: 
+        // 'teacherid	password	firstname	lastname	email	address	contactno	classid'
+        // define expected header
+        const expectedHeader = ['teacherid', 'password', 'firstname', 'lastname', 'email', 'address', 'contactno', 'classid'];
+        // check if correct number of headers
+        if (column.length !== expectedHeader.length) {
+          alert('CSV file has incorrect number of headers. Please check the columns');
+          return;
+        }
+        // check if correct name for headers
+        const isHeaderCorrect = column.every((col, index) => col === expectedHeader[index]);
+        if (!isHeaderCorrect) {
+          alert('CSV file has incorrect headers. Please check the column order and spellings.');
+          return;
+        }
+        // Second validation
+        // Validate, check if userid already exists
+        // Validate, check if classid belongs to their school
+        const schoolid = localStorage.getItem('schoolid');
+        const submittedTeacherIds = data.map((item) => item.teacherid);
+        const submittedClassIds = data.map((item) => item.classid);
+
+        axios.post('https://lagj9paot7.execute-api.ap-southeast-1.amazonaws.com/dev/api/schadm-uploadteachervalidation', {
+          si: schoolid,
+          sti: submittedTeacherIds,
+          sci: submittedClassIds,
+        })
+        .then(firstCheckRes=> {
+          if(firstCheckRes.data.success) {
+            // Passed validation, proceed to upload teacher details into DB
+            axios.post('https://lagj9paot7.execute-api.ap-southeast-1.amazonaws.com/dev/api/schadm-uploadteacher', {
+              data: data,
+              si: schoolid
+            })
+            .then(uploadRes=>{
+              if (uploadRes.data.success) {
+                alert('Teacher details uploaded successfully. Check the Teacher tab')
+              } else {
+                alert(uploadRes.data.errlog)
+              }
+            })
+          } else {
+            alert(firstCheckRes.data.errlog)
+          }
+        })
       }
   }
-  //
   //  UPLOAD FUNCTION END //
 
   return (
@@ -77,64 +126,97 @@ export default function UploadTeacher() {
           accept=".csv"
           onChange={handleCSV}
           className='w-50'/>
-        <div style={{marginTop: '40px'}} className='w-50'>
-          <CButton 
-            onClick={handleSubmit}
-            style ={{'background': '#56844B', width: '50%'}}
-            >
-            Submit
-          </CButton>
-        </div>
+
+          <div style={{marginTop: '40px', display: 'flex', justifyContent: 'space-between'}} className='w-50'>
+            <CButton 
+              onClick={handlePreview}
+              style ={{'background': '#56844B', width: '45%'}}
+              >
+              Preview
+            </CButton>
+            <CButton 
+              onClick={handleSubmit}
+              style ={{'background': '#56844B', width: '45%'}}
+              >
+              Submit
+            </CButton>
+          </div>
       </CContainer>
 
       <CContainer className='px-4 py-2 pb-5'>
         {data.length == 0 ? '' : 
-          <Typography variant="lead" className="text-center">
-            Number of teacher data registered after upload: {numberOfRows} 
+          <Typography className="px-5">
+            Number of teacher data registered after upload: {numberOfRows}
           </Typography>
         }
 
-        <Typography variant="h5" className="pt-4 pb-5 text-center">
-          How your csv should look like for uploading teacher details
+        <Typography className="px-5 pb-5">
+          Notice: Any row(s) in the CSV that have empty values will be skipped 
         </Typography>
 
         <CardHeader color="blue-gray" className="pb-2">
           <img
-            src=""  // put S3 link here
+            src=""  // link the png from assets here
             alt="Teacher CSV sample image"
           />
         </CardHeader>
-
-        <Typography variant="lead" color="red" className="px-5 pt-5">
-          Notice: Any row(s) in the CSV that have empty values will be skipped 
-        </Typography>
-
-        {/* This table is for us to see if values from csv have been parsed properly or not only, delete later*/}
-        {/* {data.length == 0 ? '' : (
-          <>
-            <table >
-              <thead>
-                <tr>
-                  {column.map((col, idx) => (
-                    <th key={idx}>{col}</th>
-                  ))}
-                </tr>
-              </thead>
+        
+        {/* Preview table */}
+        {tableVisible && (
+        <div className='pt-5'>
+          <Card className="h-full w-full overflow-scroll">
+            <table className="w-full min-w-max table-auto text-left">
               <tbody>
-                {value.map((val, idx) => (
-                  <tr key={idx}>
-                    {val.map((v,i) => (
-                      <td key={i}>{v}</td>
-                    ))}
+                {data.map(({ teacherid, password, firstname, lastname, email, address, contactno, classid }, index) => (
+                  <tr key={teacherid} className="even:bg-blue-gray-50/50">
+                    <td className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {teacherid}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {password}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {firstname}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {lastname}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {email}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {address}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {contactno}
+                      </Typography>
+                    </td>
+                    <td className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {classid}
+                      </Typography>
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
-          </>
-          )
-        } */}
+          </Card>
+        </div>
+        )}
       </CContainer>
     </>
   )
-
 }
